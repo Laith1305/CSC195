1bb0101f4a57db54d37659cf4c47d403e5963205
lalchokhachi <laithzca05@gmail.com>
2025-04-04 13:23:38 -0600

    Added Employee class for Payroll Program

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include "Student.h"
+#include <string>
 using namespace std;
 
+
+struct Person
+{
+
+public:
+    float weight;
+    int age;
+
+    
+    void f() {}
+
+};
+
 int main() {
+
+    f();
+
+    Student student;
+    student.SetName("BOB");
+    student.Write();
+
+    Person person;
+    person.age = 19;
+
+    unsigned int i = 82;
+    std::cout << i << std::endl;
+    std::cout << sizeof(char) << std::endl;
+    std::cout << sizeof(Person) << std::endl;
+
+    cout << ++i << endl;
+
+
+    
+    char c = 'A';
+
+    cout << &i << endl;
+
+
+
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
     return 0;
---
+++ HelloWorld/Student.cpp
@@ -0,0 +1,13 @@
+#include "Student.h"
+#include <iostream>
+
+void f() {
+
+	std::cout << "hello\n";
+
+}
+
+
+void Student::Write() {
+	std::cout << m_name << std::endl;
+}
